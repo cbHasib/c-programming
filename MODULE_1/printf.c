7e88fc83d7ff9ae75458916d895dce75bb843bf1
cbHasib <hasibul.pbn@gmail.com>
2023-04-02 20:00:03 +0600

    variable and buildin function

---
+++ MODULE_1/printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world!\nLove to do programming\t\t\tNew Tab ");
+    return 0;
+}
---
+++ MODULE_1/variable.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int rahim = 10;
+    float karim = 20.6;
+    char hasib = 'Z';
+    printf("%d %0.1f %c", rahim, karim, hasib);
+    return 0;
+}
