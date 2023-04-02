0e7728df13e7085588b76264abdc140462892a89
cbHasib <hasibul.pbn@gmail.com>
2023-04-02 20:24:42 +0600

    output concept

---
+++ MODULE_1/output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    // int rahim, karim;
+    // scanf("%d %d", &rahim, &karim);
+    // printf("%d %d", rahim, karim);
+
+    int a, b;
+    char p;
+    scanf("%d%% %d%%", &a, &b);
+    printf("%d%% %d%%", a, b);
+
+    return 0;
+}
